e4c642a6bed81f9e2ccbc9d258f0ea6d0695d6c1
azezmohammed <muhamadaziz047@gmail.com>
2021-02-09 14:15:08 +0700

    first commit

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       String myString = scanner.next();
+       int myInt = scanner.nextInt();
+       int secondInt = scanner.nextInt();
+       scanner.close();
+       
+       System.out.println(myString);
+       System.out.println(myInt);
+       System.out.println(secondInt);
+    }
+}
